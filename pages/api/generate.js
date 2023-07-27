a7ca53a0d308546cfc9d5536ab90df77a20d9e77
ArturT <mr.arturio@gmail.com>
2023-07-27 12:25:21 -0400

    Ai generates some text using name input, still in work

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,11 +15,11 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const name = req.body.name || '';
+  if (name.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid name",
       }
     });
     return;
@@ -27,9 +27,9 @@ export default async function (req, res) {
 
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+      model: "gpt-3.5-turbo",
+      prompt: generatePrompt(name),
+      temperature: 0.9,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
@@ -48,15 +48,8 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(name) {
+  const capitalizedName =
+    name[0].toUpperCase() + name.slice(1).toLowerCase();
+  return `Create a story for the kid named ${capitalizedName}, kid is 4 years old. Story should be funny and about school. Sory shold be 2 minute read long.`;
 }
