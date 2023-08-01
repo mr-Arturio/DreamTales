20c48de38011a1000d2c1375b17a785cd1f0629b
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-07-31 21:18:40 -0400

    api login page

--- pages/api/login.js
+++ pages/api/login.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
         alert('User not found.');
 
         client.release();
-        return;
+        res.status(404).send('User not found');
       }
 
       const user = result.rows[0];
@@ -28,13 +28,12 @@ export default async function handler(req, res) {
       if (!isPasswordValid) {
         alert('Invalid password.');
        
-        return;
+        res.status(400).send('Invalid Password');
       }
 
       // If authentication is successful, generate a JWT token and store it in a cookie
       const token = jwt.sign({ id: user.id}, process.env.JWT_SECRET); // Use the user's ID for generating the token
       res.status(200).json({token})
-      
 
     } catch (error){
       console.log('Login error:', error)
@@ -43,5 +42,5 @@ export default async function handler(req, res) {
   }
 
   // Handle other HTTP methods (GET, PUT, DELETE, etc.)
-  return res.status(405).end(); // Method Not Allowed
+    res.status(405).end(); // Method Not Allowed
 }
