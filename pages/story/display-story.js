94794a67bc71ad45585aa98c5041fab0ae4ecd19
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-08-03 21:44:37 -0400

    display story updated

--- pages/story/display-story.js
+++ pages/story/display-story.js
@@ -1,17 +1,87 @@
 import React from "react";
-import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const DisplayStoryPage = () => {
-  const router = useRouter();
-  const { result } = router.query; // Get the generated story from the URL query params
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch('/api/retrieveStory');
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    getData();
+  }, [])
+
+  const truncateText = (text, maxLength) => {
+    return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
+  };
+
 
   return (
-    <div>
-      <h1>Your Story</h1>
-      {result ? (
-        <textarea rows={50} value={result} readOnly />
-      ) : (
-        <p>No story available.</p>
+    <div className='grid grid-cols-3 gap-4'>
+      {data.map((item) => (
+       
+  <>
+      {/*<!-- Component: Horizontal card--> */}
+      <div className="flex flex-col overflow-hidden bg-white rounded shadow-md text-slate-500 shadow-slate-200 sm:flex-row">
+        {/*  <!-- Image --> */}
+        {/*  <!-- Body--> */}
+        <div className="flex-1 p-6 sm:mx-6 sm:px-0">
+          <header className="flex gap-4 mb-4 h-auto">
+            <div>
+              <h3 className="text-xl font-medium text-slate-700">
+                {item.title}
+              </h3>
+              <p className="text-sm text-slate-400"> {item.created_at}</p>
+            </div>
+          </header>
+        <figure className="">
+          <img
+            src={item.photo}
+            alt="card image"
+            className="m-auto"
+          />
+        </figure>
+          <p>
+            {truncateText(item.story,150)}</p>
+        <button className="inline-flex h-10 items-center justify-center gap-2 justify-self-end whitespace-nowrap rounded px-5 text-sm font-medium tracking-wide text-emerald-500 transition duration-300 hover:bg-emerald-100 hover:text-emerald-600 focus:bg-emerald-200 focus:text-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:text-emerald-300 disabled:shadow-none disabled:hover:bg-transparent">
+            <span className="relative only:-mx-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                role="graphics-symbol"
+                aria-labelledby="title-81 desc-81"
+              >
+                <title id="title-81">Favorites</title>
+                <desc id="desc-81">
+                 Click to add to favorites page 
+                </desc>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                />
+              </svg>
+            </span>
+          </button>
+        </div>
+      </div>
+
+      {/*<!-- End Horizontal card--> */}
+    </>
+      
+
+      )
+
       )}
     </div>
   );
