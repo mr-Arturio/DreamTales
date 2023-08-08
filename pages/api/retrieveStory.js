c658f4df5c19175814a11b345f97eb9be423c0e9
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-08-07 20:42:23 -0400

    retrieve story api

--- pages/api/retrieveStory.js
+++ pages/api/retrieveStory.js
@@ -1,24 +1,16 @@
 import db from "/db/database";
-import cookie from "cookie";
-import { verify } from 'jsonwebtoken';
+
 
 export default async function connectResponse(req, res) {
   try {
     const client = await db.connect()
     const result = await client.query('SELECT * FROM stories;');
-    const cookieValue = await cookie.parse(req.headers.cookie);
-    console.log('cookie value', cookieValue)
-    const token = await verify(cookieValue.UserCookie, process.env.JWT_SECRET, (error, decoded) => { 
-      console.log("decoded--->", decoded)
-    });
     
-    // console.log('REQ--->',verify(cookie.parse(req.headers.cookie), process.env.JWT_SECRET))
     const story = result.rows;
     client.release();
     res.status(200).json(story)
   } catch (error) {
     res.status(500).json({ error: 'Error fetching data from database' })
   }
+  
 }
-
-
