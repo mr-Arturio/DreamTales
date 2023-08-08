cd93cc6ef616ef37df365a1b359f35772d881d17
ArturT <mr.arturio@gmail.com>
2023-08-08 11:17:27 -0400

    save imgs to the local folder

--- pages/api/img-generate.js
+++ pages/api/img-generate.js
@@ -1,4 +1,5 @@
-// generateImage.js
+import fs from "fs";
+import path from "path";
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
@@ -21,7 +22,22 @@ export async function generateImage() {
     });
 
     const imageUrl = completion.data.data[0].url;
-    return imageUrl;
+    console.log("image URL:", imageUrl);
+
+    // Fetch the image data using the fetch function
+    const response = await fetch(imageUrl);
+    const imageArrayBuffer = await response.arrayBuffer();
+
+    // Convert the ArrayBuffer to a Buffer
+    const imageBuffer = Buffer.from(imageArrayBuffer);
+
+    // Save the image data to a file
+    const imageFileName = `generated_image_${Date.now()}.png`;
+    const imagePath = path.join(process.cwd(), "public/images", imageFileName);
+    fs.writeFileSync(imagePath, imageBuffer);
+
+    return `/images/${imageFileName}`;
+
   } catch (error) {
     if (error.response) {
       console.error(error.response.status, error.response.data);
