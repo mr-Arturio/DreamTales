e91c0ed372889395bfea0b02dc42a779ff74d5ac
ArturT <mr.arturio@gmail.com>
2023-08-10 19:16:00 -0400

    basic style of the Home page

--- src/components/home-page.jsx
+++ src/components/home-page.jsx
@@ -3,18 +3,17 @@ import Link from "next/link";
 
 export const HomePage = () => (
   <div className="home_body">
-    <h1>Home Page</h1>
-    <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
-    <Link href="/story/" passHref>
-    <button>Create story</button>
-    </Link>
+    <div className="home_content">
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+        est laborum.
+      </p>
+      <Link href="/story/" passHref>
+        <button>Create story</button>
+      </Link>
+    </div>
   </div>
 );
