4e58ff9af54b2633638938f5a87d6933ab86075b
ArturT <mr.arturio@gmail.com>
2023-08-13 16:54:47 -0400

    some progress in story form page

--- pages/story/story-form.js
+++ pages/story/story-form.js
@@ -109,9 +109,23 @@ const StoryForm = ({
         onSubmit={onSubmit}
         className="flex flex-col lg:flex-row lg:flex-wrap lg:justify-between"
       >
-        <div className="w-full lg:w-1/4 flex justify-center">
-          <InputElement />
-        </div>
+        <div className="w-full lg:w-1/4 flex justify-center relative ">
+        <input
+          id="childName"
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          value={childName}
+          onChange={(e) => setChildName(e.target.value)}
+          className="peer relative h-10 w-full mb-10 mt-10 rounded border border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+        />
+        <label
+          htmlFor="childName"
+          className="absolute left-2 -top-2 z-[1] mb-10 mt-10 cursor-text px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+        >
+          Your name
+        </label>
+      </div>
 
         <div className="w-full lg:w-1/4 flex justify-center">
           <DropdownMenu
@@ -132,41 +146,42 @@ const StoryForm = ({
         </div>
 
         <div className="w-full lg:w-1/4 relative flex justify-center">
-          <button
-            className="inline-flex items-center justify-center h-10 gap-2 px-5 text-sm font-medium tracking-wide transition duration-300 rounded shadow-md focus-visible:outline-none justify-self-center whitespace-nowrap bg-emerald-50 text-emerald-500 shadow-emerald-100 hover:bg-emerald-100 hover:text-emerald-600 hover:shadow-md hover:shadow-emerald-100 focus:bg-emerald-200 focus:text-emerald-700 focus:shadow-md focus:shadow-emerald-100 disabled:cursor-not-allowed disabled:border-emerald-100 disabled:bg-emerald-100 disabled:shadow-none"
-            onClick={handleButtonClick}
-          >
-            <span>Add Secondary Hero</span>
-          </button>
-       
-        {isDropdownOpen && (
-           <div className="absolute top-10 left-0 w-full">
-            <DropdownMenu
-              id="secondaryHero"
-              buttonLabel={"Pick One"}
-              navigationItems={pickSecondaryHero}
-              setFunction={setSecondaryHero}
-            />
+  <button
+    className="inline-flex items-center justify-center mb-10 mt-10 h-10 gap-2 px-5 text-sm font-medium tracking-wide transition duration-300 rounded shadow-md focus-visible:outline-none justify-self-center whitespace-nowrap bg-emerald-50 text-emerald-500 shadow-emerald-100 hover:bg-emerald-100 hover:text-emerald-600 hover:shadow-md hover:shadow-emerald-100 focus:bg-emerald-200 focus:text-emerald-700 focus:shadow-md focus:shadow-emerald-100 disabled:cursor-not-allowed disabled:border-emerald-100 disabled:bg-emerald-100 disabled:shadow-none"
+    onClick={handleButtonClick}
+  >
+    <span>Add Secondary Hero</span>
+  </button>
 
-            <div className="relative my-6">
-              <input
-                id="secondaryHeroName"
-                type="text"
-                name="secondaryHeroName"
-                value={secondaryHeroName}
-                onChange={(e) => setSecondaryHeroName(e.target.value)}
-                class="relative w-full h-10 px-4 text-sm placeholder-transparent transition-all border rounded outline-none focus-visible:outline-none peer border-slate-200 text-slate-500 autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
-              />
-              <label
-                for="secondaryHeroName"
-                class="cursor-text peer-focus:cursor-default absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
-              >
-                Name
-              </label>
-            </div>
-          </div>
-        )}
-         </div>
+  {isDropdownOpen && (
+    <div className="absolute top-[3rem] left-0 mb-5 mt-5 w-full flex flex-col items-center">
+      <DropdownMenu
+        id="secondaryHero"
+        buttonLabel={"Pick One"}
+        navigationItems={pickSecondaryHero}
+        setFunction={setSecondaryHero}
+      />
+
+      <div className="relative my-6">
+        <input
+          id="secondaryHeroName"
+          type="text"
+          name="secondaryHeroName"
+          value={secondaryHeroName}
+          onChange={(e) => setSecondaryHeroName(e.target.value)}
+          className="relative w-full h-10 px-4 text-sm placeholder-transparent transition-all border rounded outline-none focus-visible:outline-none peer border-slate-200 text-slate-500 autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+        />
+        <label
+          htmlFor="secondaryHeroName"
+          className="cursor-text peer-focus:cursor-default absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+        >
+          Name
+        </label>
+      </div>
+    </div>
+  )}
+</div>
+
         <div className="w-full lg:w-1/4 flex justify-center">
           <DropdownMenu
             id="gender"
@@ -175,7 +190,7 @@ const StoryForm = ({
             setFunction={setGender}
           />
         </div>
-        <div className="w-full lg:w-1/4 flex justify-center">
+        <div className="w-full lg:w-1/4 flex justify-center mt-10">
           <RangeSlider
             label="Age"
             sliderValue={age}
@@ -186,7 +201,7 @@ const StoryForm = ({
           />
         </div>
 
-        <div className="w-full lg:w-1/4 flex justify-center">
+        <div className="w-full lg:w-1/4 flex justify-center mt-10">
           <RangeSlider
             label="Time"
             sliderValue={time}
@@ -197,6 +212,8 @@ const StoryForm = ({
           />
         </div>
 
+        <div className="w-full lg:w-1/4 flex justify-center"></div>
+
         <div className="w-full lg:w-1/4 flex justify-center">
           <DropdownMenu
             id="language"
@@ -206,7 +223,7 @@ const StoryForm = ({
           />
         </div>
 
-        <div className="w-full lg:w-1/4 flex justify-center">
+        <div className="w-full lg:w-1/4 flex items-center justify-center">
           <Checkbox />
         </div>
       </div>
@@ -214,7 +231,7 @@ const StoryForm = ({
         <button
           type="button"
           onClick={onSubmit}
-          className="inline-flex items-center justify-center h-12 gap-2 px-6 text-sm font-medium tracking-wide text-white transition duration-300 rounded focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none"
+          className="inline-flex items-center justify-center mb-10 mt-10 h-12 gap-2 px-6 text-sm font-medium tracking-wide text-white transition duration-300 rounded focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none"
         >
           <span>Generate the Story</span>
         </button>
