ef6716976001dcd8c4a294ab2cb2e6ef80a51c35
ArturT <mr.arturio@gmail.com>
2023-08-14 23:01:40 -0400

    add margin to img

--- pages/story/display-last-story.js
+++ pages/story/display-last-story.js
@@ -39,19 +39,19 @@ const DisplayStoryPage = () => {
     <div className="flex flex-col items-center p-6 bg-cover bg-center bg-no-repeat min-h-screen"     style={{
       backgroundImage:
         'url("/docs/design/Backgrounds/last.svg")'}}>
-      <h1 className="text-3xl font-bold mb-4" style={{ fontFamily: "Garet, sans-serif"}}>{title.title}</h1>
+      <h1 className="text-4xl font-bold font-comic-sans mt-10 mb-5">{title.title}</h1>
       {loading ? (
         <p>Loading...</p>
       ) : (
         <div className="flex flex-row justify-evenly lg:flex-row items-center lg:items-center space-y-4 lg:space-y-0">
-          <div className="w-full lg:w-3/5 p-4 border rounded-md shadow-lg  bg-cover bg-center bg-no-repeat bg-slate-200" style={{ fontFamily: "Garet, sans-serif", fontSize: "1.4rem", marginTop: "2rem" }}>
+          <div className="w-full lg:w-4/5 p-4 border rounded-md shadow-lg  bg-cover bg-center bg-no-repeat bg-slate-200" style={{ fontFamily: "Garet, sans-serif", fontSize: "1.4rem", marginTop: "2rem" }}>
             <div className="float-right mr-4 mb-4 lg:mb-0 lg:mr-0">
               <Image
                 src={generatedImage}
                 alt="Story Image"
                 width={600}
                 height={150}
-                className="overflow-hidden"
+                className="overflow-hidden ml-7 mb-3"
               />
             </div>
             <p>{animatedText}</p>
