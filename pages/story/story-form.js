326420a91a1ca815063dd60d576456e831973229
ArturT <mr.arturio@gmail.com>
2023-08-17 10:48:47 -0400

    improve smaller screen readability

--- pages/story/story-form.js
+++ pages/story/story-form.js
@@ -113,7 +113,7 @@ const StoryForm = ({
     <div className="bg-blue2  border border-blue2 bg-opacity-40 p-4 ml-40 mr-40 mb-10 rounded-lg shadow-md">
       <form
         onSubmit={onSubmit}
-        className="flex flex-col lg:flex-row lg:flex-wrap lg:justify-between ml-52 mr-52"
+        className="flex flex-col lg:flex-row lg:flex-wrap lg:justify-between mx-auto lg:ml-12 lg:mr-12"
       >
         <div className="w-full lg:w-1/4 flex justify-center relative ">
           <input
@@ -123,11 +123,11 @@ const StoryForm = ({
             placeholder="Enter Name"
             value={childName}
             onChange={(e) => setChildName(e.target.value)}
-            className="peer relative h-10 w-full mb-10 mt-10 rounded border border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+            className="peer relative h-10 w-80 mb-10 mt-10 rounded border border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
           />
           <label
             htmlFor="childName"
-            className="absolute left-2 -top-2 z-[1] mb-10 mt-10 cursor-text px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+            className="absolute left-7 -top-2 z-[1] mb-10 mt-10 cursor-text px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
           >
             Your name
           </label>
