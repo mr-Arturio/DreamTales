0c98afb6bb1ff37deae1b75094c22f7dbab84f45
ArturT <mr.arturio@gmail.com>
2023-08-17 12:10:09 -0400

    fix id

--- pages/register.js
+++ pages/register.js
@@ -69,14 +69,14 @@ export default function Register() {
               <input
                 value={name}
                 onChange={(event) => setName(event.target.value)}
-                id="id-b03"
+                id="id-b04"
                 type="text"
-                name="id-b03"
+                name="id-b04"
                 placeholder="your name"
                 className="peer relative h-10 w-full rounded border border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
               />
               <label
-                htmlFor="id-b03"
+                htmlFor="id-b04"
                 className="absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
               >
                 Your name
