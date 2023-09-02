aa602dfee6e293b01ec7b32b20f70be792acb6c7
ArturT <mr.arturio@gmail.com>
2023-09-01 23:27:21 -0400

    heroku database

--- db/database.js
+++ db/database.js
@@ -1,27 +1,11 @@
-// const { Pool } = require('pg');
-
-// const db = new Pool({
-//   connectionString: process.env.DATABASE_URL, // Use the Heroku DATABASE_URL environment variable
-//   ssl: {
-//     rejectUnauthorized: false, // Disable SSL verification for development
-//   },
-// });
-
-// db.connect()
-
-// module.exports = db;
-
 const { Pool } = require('pg');
 
-const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-};
-
-const db = new Pool(dbParams);
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL, // Use the Heroku DATABASE_URL environment variable
+  ssl: {
+    rejectUnauthorized: false, // Disable SSL verification for development
+  },
+});
 
 db.connect()
 
